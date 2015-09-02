7d9c62780520fd76b7b5d72a321ef65d04d60dfc
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-01 18:42:08 -1000

    completed multiple challenge

--- multiples.js
+++ multiples.js
@@ -5,8 +5,13 @@
  */
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
-
+  for (var i = 0; i < n; i++) {
+    if (i % 5 === 0) {
+      sum += i;
+    }else if (i % 3 === 0) {
+      sum += i;
+    }
+  }
   // do your work here
-
-  return sum;
+   return sum;
 };
